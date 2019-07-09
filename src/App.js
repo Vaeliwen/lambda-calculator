a4deaf6b80cd736bb6070b4296d22c4e10070aa5
Vaeliwen <zaman@imbriaco.com>
2019-07-09 15:42:46 -0400

    Created Numbers and NumberButton components.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
+import { numbers, operators, specials } from './data'
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -12,6 +13,9 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
+  const [number, setNumber] = useState(numbers)
+  const [operator, setOperator] = useState(operators)
+  const [special, setSpecial] = useState(specials)
 
   return (
     <div className="container">
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { numbers } from '../../../data'
+import NumberButton from './NumberButton'
 
 //import any components needed
 
@@ -6,11 +8,12 @@ import React from "react";
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [nums, setNums] = useState(numbers)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/nums.map((num) => {return <NumberButton number={num} />})}
     </div>
   );
 };
